69f91905553b51f4a1ba4ef1c299e39c30ac808f
윤동휘 <xorms1119@naver.com>
2024-04-29 16:24:40 +0900

    feat: Add BaseEntity setting

---
+++ backend/src/main/java/com/ssafy/mimo/common/BaseDeletableEntity.java
@@ -0,0 +1,31 @@
+package com.ssafy.mimo.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseDeletableEntity extends BaseEntity {
+
+	private Boolean isActive = true;
+
+	@CreatedDate
+	@Column(columnDefinition = "datetime(0) default now(0)", nullable = false, updatable = false)
+	private LocalDateTime registeredDttm;
+
+	private LocalDateTime unregisteredDttm;
+}
---
+++ backend/src/main/java/com/ssafy/mimo/common/BaseUnregisterableEntity.java
@@ -0,0 +1,4 @@
+package com.ssafy.mimo.common;
+
+public class BaseUnregisterableEntity {
+}
