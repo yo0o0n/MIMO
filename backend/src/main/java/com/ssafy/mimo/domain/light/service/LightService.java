d4172fd2c71d76e79bb4201580f45da44436ee15
김준하 <kimjunha1575@gmail.com>
2024-05-02 15:05:40 +0900

    feat: Implement light off API
    Change-Id: I8c97c2321a5378d9ebb698422c5ddc103c3da495

--- backend/src/main/java/com/ssafy/mimo/domain/light/service/LightService.java
+++ backend/src/main/java/com/ssafy/mimo/domain/light/service/LightService.java
@@ -2,14 +2,16 @@ package com.ssafy.mimo.domain.light.service;
 
 import com.ssafy.mimo.domain.hub.entity.Hub;
 import com.ssafy.mimo.domain.hub.service.HubService;
-import com.ssafy.mimo.domain.hub.service.UserService;
 import com.ssafy.mimo.domain.light.entity.Light;
 import com.ssafy.mimo.domain.light.repository.LightRepository;
+import com.ssafy.mimo.socket.global.SocketController;
 import com.ssafy.mimo.user.entity.User;
+import com.ssafy.mimo.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.net.Socket;
 import java.time.LocalDateTime;
 
 @Service
@@ -19,6 +21,7 @@ public class LightService {
     private final LightRepository lightRepository;
     private final UserService userService;
     private final HubService hubService;
+    private final SocketController socketController;
     private Light findLightById(Long lightId) {
         return lightRepository.findById(lightId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 ID를 가진 조명이 존재하지 않습니다."));
@@ -58,5 +61,29 @@ public class LightService {
             return "Failed to unregister light!";
         }
         return "Light unregistered!";
+    }
+    private String updateLightNickname(Long lightId, String nickname) {
+        Light light = findLightById(lightId);
+        try {
+            light.setNickname(nickname);
+            lightRepository.save(light);
+        } catch (Exception e) {
+            return "Failed to update light nickname!";
+        }
+        return "Light nickname updated!";
+    }
+    private String turnOffLight(Long lightId) {
+        Light light = findLightById(lightId);
+        Hub hub = light.getHub();
+        if (hub != null) {
+            try (Socket socket = socketController.getSocket(hub.getId())) {
+                String message = "{type:light, requestName:setStateOff}";
+                socket.getOutputStream().write(message.getBytes());
+                return "Light turned off!";
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Failed to connect to hub!");
+            }
+        }
+        throw new IllegalArgumentException("Light is not connected to a hub!");
     }
 }
