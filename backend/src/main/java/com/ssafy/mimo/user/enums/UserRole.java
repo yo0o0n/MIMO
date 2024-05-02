d4e8cb2a6d464241759bada5fb3ff8a771d1d0a1
윤동휘 <xorms1119@naver.com>
2024-05-02 16:32:17 +0900

    feat: Add kakao login enums

---
+++ backend/src/main/java/com/ssafy/mimo/user/enums/UserRole.java
@@ -0,0 +1,16 @@
+package com.ssafy.mimo.user.enums;
+
+public enum UserRole {
+	USER("user"),
+	ADMIN("admin");
+
+	private final String role;
+
+	UserRole(String role) {
+		this.role = role;
+	}
+
+	public String getRole() {
+		return role;
+	}
+}
