93ba9f65350649abca87ee89ba0fcadd6f1e14b7
김준하 <kimjunha1575@gmail.com>
2024-05-02 16:11:19 +0900

    feat: Add health checker for jenkins
    Change-Id: Ie1265c7561433ab69ab1bcd7acc3a64efae41a74

---
+++ backend/src/main/java/com/ssafy/mimo/healthchecker/HealthCheckerController.java
@@ -0,0 +1,14 @@
+package com.ssafy.mimo.healthchecker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/check")
+public class HealthCheckerController {
+    @GetMapping
+    public String check() {
+        return "I'm alive!";
+    }
+}
