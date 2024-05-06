3f872d9d7ff02de4c0d997b450c3d5bfcaf3c43d
윤동휘 <xorms1119@naver.com>
2024-05-06 18:56:02 +0900

    feat: Add AuthController swagger setting

--- backend/src/main/java/com/ssafy/mimo/user/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/mimo/user/controller/AuthController.java
@@ -11,8 +11,11 @@ import com.ssafy.mimo.user.service.JwtTokenProvider;
 import com.ssafy.mimo.user.service.KakaoAuthService;
 import com.ssafy.mimo.user.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "로그인 컨트롤러", description = "로그인 및 회원가입 관련 기능이 포함되어 있음")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -23,6 +26,7 @@ public class AuthController {
 	private final JwtTokenProvider jwtTokenProvider;
 
 	// 카카오 로그인을 위해 회원가입 여부 확인, 이미 회원이면 Jwt 토큰 발급
+	@Operation(summary = "카카오 로그인")
 	@PostMapping("")
 	public TokenResponseDto login(@RequestBody LoginRequestDto loginRequestDto) {
 		Long userId = kakaoAuthService.isSignedUp(loginRequestDto.getAccessToken()); // 유저 고유번호 추출
