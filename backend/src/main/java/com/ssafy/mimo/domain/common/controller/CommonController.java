1611ead5c26f5bc11738970279eb426f674c8c48
김준하 <kimjunha1575@gmail.com>
2024-05-07 13:14:58 +0900

    feat: Add skeleton for manual control
    Change-Id: I3e26d49c196b16336f1105ccc0f0974a33ab3661

---
+++ backend/src/main/java/com/ssafy/mimo/domain/common/controller/CommonController.java
@@ -0,0 +1,21 @@
+package com.ssafy.mimo.domain.common.controller;
+
+import com.ssafy.mimo.domain.common.dto.SocketRequestDto;
+import com.ssafy.mimo.domain.common.service.CommonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/control")
+@RequiredArgsConstructor
+public class CommonController {
+    private final CommonService commonService;
+    @PostMapping
+    public ResponseEntity<String> controlDevice(@RequestBody SocketRequestDto SocketRequestDto) {
+        return ResponseEntity.ok(commonService.controlDevice(SocketRequestDto));
+    }
+}
