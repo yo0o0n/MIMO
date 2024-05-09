766008835c543131217a32a15e61ade7f2936d67
김준하 <kimjunha1575@gmail.com>
2024-05-09 16:57:52 +0900

    style: Add annotation for swagger
    Change-Id: Iaf4e2cd27b5a2acda726ff26db376403080e27aa

--- backend/src/main/java/com/ssafy/mimo/domain/common/controller/CommonController.java
+++ backend/src/main/java/com/ssafy/mimo/domain/common/controller/CommonController.java
@@ -2,6 +2,8 @@ package com.ssafy.mimo.domain.common.controller;
 
 import com.ssafy.mimo.domain.common.dto.ManualControlRequestDto;
 import com.ssafy.mimo.domain.common.service.CommonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "기기 수동제어 컨트롤러", description = "앱을 통한 기기의 수동제어 관련 기능이 포함되어 있음")
 @RestController
 @RequestMapping("/api/v1/control")
 @RequiredArgsConstructor
 public class CommonController {
     private final CommonService commonService;
+    @Operation(summary = "기기 수동제어")
     @PostMapping
     public ResponseEntity<String> controlDevice(@RequestBody ManualControlRequestDto manualControlRequestDto) {
         return ResponseEntity.ok(commonService.controlDevice(manualControlRequestDto));
