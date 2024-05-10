19555f8b48aac94bad506ff0524335c6ff11a14e
이수지 <suzy0120@naver.com>
2024-05-10 13:10:52 +0900

    style: Add annotation for swagger

--- backend/src/main/java/com/ssafy/mimo/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/mimo/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.ssafy.mimo.user.controller;
 
 import com.ssafy.mimo.user.dto.MyInfoResponseDto;
 import com.ssafy.mimo.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "사용자 컨트롤러", description = "사용자 정보 일치 기능이 포함되어 있음")
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
@@ -16,6 +19,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "해당 사용자의 집/허브 유무 파악")
     @GetMapping("/myInfo")
     public ResponseEntity<MyInfoResponseDto> getHomeAndHubInfo(@RequestHeader("X-AUTH-TOKEN") String token) {
         Long userId = userService.getUserId(token);
