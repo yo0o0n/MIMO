1a5f454b00590bea47e5805aa7405a8e5a63f1bb
김준하 <kimjunha1575@gmail.com>
2024-05-13 18:21:13 +0900

    fix: Fix manual controller
    Change-Id: I110e39f7defbec705b3420668dfd721797db2f10

--- backend/src/main/java/com/ssafy/mimo/domain/common/service/CommonService.java
+++ backend/src/main/java/com/ssafy/mimo/domain/common/service/CommonService.java
@@ -1,23 +1,16 @@
 package com.ssafy.mimo.domain.common.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.ssafy.mimo.common.BaseDeviceEntity;
 import com.ssafy.mimo.domain.common.dto.ManualControlRequestDto;
 import com.ssafy.mimo.domain.curtain.service.CurtainService;
 import com.ssafy.mimo.domain.hub.entity.Hub;
 import com.ssafy.mimo.domain.lamp.service.LampService;
 import com.ssafy.mimo.domain.light.service.LightService;
-//import com.ssafy.mimo.domain.curtain.service.CurtainService;
-//import com.ssafy.mimo.domain.window.service.WindowService;
-//import com.ssafy.mimo.domain.shower.service.ShowerService;
 import com.ssafy.mimo.domain.window.service.WindowService;
 import com.ssafy.mimo.socket.global.SocketController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.net.Socket;
 
 @Service
 @RequiredArgsConstructor
@@ -30,7 +23,7 @@ public class CommonService {
     public String controlDevice(ManualControlRequestDto manualControlRequestDto) {
         String type = manualControlRequestDto.getType();
         Long deviceId = manualControlRequestDto.getDeviceId();
-        BaseDeviceEntity device = null;
+        BaseDeviceEntity device;
         switch (type) {
             case "light":
                 device = lightService.findLightById(deviceId);
@@ -41,9 +34,9 @@ public class CommonService {
             case "window":
                 device = windowService.findWindowById(deviceId);
                 break;
-//            case "curtain":
-//                device = curtainService.findCurtainById(deviceId);
-//                break;
+            case "curtain":
+                device = curtainService.findCurtainById(deviceId);
+                break;
             default:
                 return "기기를 찾을 수 없습니다. 기기를 다시 확인해 주세요.";
         }
@@ -52,10 +45,15 @@ public class CommonService {
             return "기기와 연결된 허브가 없습니다. 허브를 연결해 주세요.";
         try {
             String color = manualControlRequestDto.getData().getColor();
-            if (type.equals("light"))
-                lightService.setLightCurColor(deviceId, color);
-            else if (type.equals("lamp"))
-                lampService.setLampCurColor(deviceId, color);
+            String requestName = manualControlRequestDto.getData().getRequestName();
+            // CRUD
+            if (requestName.equals("setCurrentColor")) {
+                if (type.equals("light")) {
+                    lightService.setLightCurColor(deviceId, color);
+                } else if (type.equals("lamp")) {
+                    lampService.setLampCurColor(deviceId, color);
+                }
+            }
             SocketController.sendMessage(hub.getId(), manualControlRequestDto.toString());
         } catch (Exception e) {
             return "허브와 연결할 수 없습니다. 허브 연결을 확인해 주세요.";
