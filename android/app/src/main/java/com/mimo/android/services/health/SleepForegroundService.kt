1d994d0b6f087a0e4bdd58c9e753a5a99fada1b5
윤동휘 <xorms1119@naver.com>
2024-05-14 17:09:30 +0900

    feat: Add detect phone screen on logic

--- android/app/src/main/java/com/mimo/android/services/health/SleepForegroundService.kt
+++ android/app/src/main/java/com/mimo/android/services/health/SleepForegroundService.kt
@@ -2,7 +2,10 @@ package com.mimo.android.services.health
 
 import com.mimo.android.R
 import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,6 +16,9 @@ class SleepForegroundService: Service() {
     private val _stepFlow = MutableStateFlow<String?>(null)
     var stepFlow: StateFlow<String?> = _stepFlow
 
+    // 핸드폰 동작여부 감지하기 위한 브로드캐스트 리시버
+    private lateinit var screenOnReceiver: BroadcastReceiver
+
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
@@ -20,13 +26,32 @@ class SleepForegroundService: Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         when(intent?.action) {
             Actions.START.toString() -> start()
-            Actions.STOP.toString() -> stopSelf()
+            Actions.STOP.toString() -> {
+                stopSelf()
+                unregisterReceiver(screenOnReceiver)
+            }
         }
 
         return super.onStartCommand(intent, flags, startId)
     }
 
+    // 핸드폰 화면 켜지는 거 인식해서 동작
+    private fun initializeScreenOnReceiver() {
+        screenOnReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if (intent?.action == Intent.ACTION_USER_PRESENT) {
+                    // 여기에 "api/v1/sleep-data/phone-on" 으로 POST 요청만 보내면 됨!
+                }
+            }
+        }
+
+        val filter = IntentFilter(Intent.ACTION_USER_PRESENT)
+        registerReceiver(screenOnReceiver, filter)
+    }
+
     private fun start(){
+        initializeScreenOnReceiver()
+
         val notification = NotificationCompat
             .Builder(this, "running_channel")
             .setSmallIcon(R.drawable.ic_launcher_foreground)
