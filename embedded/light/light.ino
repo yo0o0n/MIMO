238e1a81c30454ba8d5d5efd42c086804dcc3ff0
황성재 <woakd0705@naver.com>
2024-05-14 16:55:48 +0900

    add: Add light main feature

--- embedded/light/light.ino
+++ embedded/light/light.ino
@@ -25,11 +25,28 @@
 #include <BLE2902.h>
 #include <ArduinoJson.h>
 
+enum STATE {
+  OFF = 0,
+  ON
+};
+
 BLEServer *pServer = NULL;
 BLECharacteristic * pTxCharacteristic;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint8_t txValue = 0;
+
+STATE state = OFF;
+int red = 0;
+int green = 0;
+int blue = 0;
+
+int wake_loop = 0;
+unsigned long start_time = 0ul;
+unsigned long wake_time = 0ul;
+int wake_red = 0;
+int wake_green = 0;
+int wake_blue = 0;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -49,64 +66,107 @@ class MyServerCallbacks: public BLEServerCallbacks {
     }
 };
 
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string rxValue = pCharacteristic->getValue();
+int getColor(char a, char b){
+  int ret = 0;
+  if(a <= '9'){
+    ret = a - '0';
+  }
+  else{
+    ret = a - 'A' + 10;
+  }
+  ret *= 16;
 
-      if (rxValue.length() > 0) {
-        Serial.println("*********");
-        Serial.print("Received Value: ");
-        
-        // recv json data parsing
-        JsonDocument root;
-        deserializeJson(root, rxValue);
-        
-        std::string request_name = root["requestName"];
-        if(request_name.compare("setDefaultColor") == 0){ // light set default color
+  if(b <= '9'){
+    ret += (b - '0');
+  }
+  else{
+    ret += (b - 'A' + 10);
+  }
+
+  return ret;
+}
+
+void loopfunc(){
+  unsigned long cur_time = millis();
+  unsigned long diff = cur_time - start_time;
+  double percent = (double)diff / wake_time;
+  percent *= percent;
+  Serial.println(percent);
+  if(percent >= 1){
+    percent = 1.0;
+  }
+  ledcWrite(0, wake_red * percent);
+  ledcWrite(1, wake_green * percent);
+  ledcWrite(2, wake_blue * percent);
+  if(diff >= wake_time){
+    wake_loop = 0;
+  }
+}
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    std::string rxValue = pCharacteristic->getValue();
+
+    if (rxValue.length() > 0) {
+      Serial.println("*********");
+      Serial.print("Received Value: ");
+      for(int i = 0; i < rxValue.length(); i++){
+        Serial.print(rxValue[i]);
+      }
+      Serial.println();
+      Serial.println("*********");
+
+      JsonDocument root;
+      deserializeJson(root, rxValue);
+      std::string request_name = root["requestName"];
+      if(request_name.compare("getId") == 0){
+        root["machineType"] = "light";
+        String output;
+        serializeJson(root, output);
+        pTxCharacteristic->setValue((uint8_t*)output.c_str(), output.length());
+        pTxCharacteristic->notify();
+      }
+      else{
+        wake_loop = 0;
+        if(request_name.compare("setCurrentColor") == 0){
           std::string color = root["color"];
-          int red = color[0];
-          int green = color[1];
-          int blue = color[2];
+
+          state = ON;
+          red = getColor(color[0], color[1]);
+          green = getColor(color[2], color[3]) * 0.5;
+          blue = getColor(color[4], color[5]) * 0.5;
+          Serial.printf("%d %d %d\n", red, green, blue);
           ledcWrite(0, red);
           ledcWrite(1, green);
           ledcWrite(2, blue);
-          Serial.printf("%d,%d,%d", red, green, blue);
         }
-        else if(request_name.compare("setCurrentColor") == 0){  // light set current color
-          std::string color = root["color"];
-          int red = color[0];
-          int green = color[1];
-          int blue = color[2];
-          ledcWrite(0, 255);
-          ledcWrite(1, 255);
-          ledcWrite(2, 255);
-          Serial.printf("%d,%d,%d", red, green, blue);
-        }
-        else if(request_name.compare("setWakeupColor") == 0){ // light set wakeup color
-          std::string color = root["color"];
-          int time = root["time"];
-          time *= 60;
-          // 시간에 따라 값 변화
-          for(int i = 0; i <= time; i++){   // increase illuminance for time minutes
-            int red = color[0] * i / time;
-            int green = color[1] * i / time;
-            int blue = color[2] * i / time;
-            ledcWrite(0, red);
-            ledcWrite(1, green);
-            ledcWrite(2, blue);
-            delay(17);
-          }
-        }
+        else if(request_name.compare("setWakeupColor") == 0){
+          start_time = millis();
+          wake_time = root["time"];
+          wake_time *= 1000;
 
-        /*
-        for (int i = 0; i < rxValue.length(); i++)
-          Serial.print(rxValue[i]);
-        */
-
-        Serial.println();
-        Serial.println("*********");
+          std::string color = root["color"];
+          wake_red = getColor(color[0], color[1]);
+          wake_green = getColor(color[2], color[3]) * 0.5;
+          wake_blue = getColor(color[4], color[5]) * 0.5;
+          wake_loop = 1;
+        }
+        else if(request_name.compare("setStateOff") == 0){
+          state = OFF;
+          ledcWrite(0, 0);
+          ledcWrite(1, 0);
+          ledcWrite(2, 0);
+        }
+        else if(request_name.compare("getState") == 0){
+          root["state"] = (state == OFF ? "off" : "on");
+          String output;
+          serializeJson(root, output);
+          pTxCharacteristic->setValue((uint8_t*)output.c_str(), output.length());
+          pTxCharacteristic->notify();
+        }
       }
     }
+  }
 };
 
 
@@ -147,20 +207,32 @@ void setup() {
   
   // led pin pwm setting
   ledcSetup(0, 5000, 8);
-  ledcAttachPin(25, 0);
   ledcSetup(1, 5000, 8);
-  ledcAttachPin(26, 1);
   ledcSetup(2, 5000, 8);
-  ledcAttachPin(27, 2);
+  ledcAttachPin(21, 0);
+  ledcAttachPin(19, 1);
+  ledcAttachPin(18, 2);
+  ledcAttachPin(5, 0);
+  ledcAttachPin(17, 1);
+  ledcAttachPin(16, 2);
+  ledcAttachPin(4, 0);
+  ledcAttachPin(2, 1);
+  ledcAttachPin(15, 2);
 }
 
 void loop() {
-    if (deviceConnected) {
-        pTxCharacteristic->setValue(&txValue, 1);
-        pTxCharacteristic->notify();
-        txValue++;
-		delay(10); // bluetooth stack will go into congestion, if too many packets are sent
-	}
+  if (deviceConnected) {
+    if(wake_loop){
+      loopfunc();
+    }
+    delay(100);
+    // if(Serial.available()){
+    //   String input = Serial.readString();
+    //   input[input.length()-1] = 0u;
+    //   pTxCharacteristic->setValue((uint8_t*)input.c_str(), input.length());
+    //   pTxCharacteristic->notify();
+    // }
+  }
 
     // disconnecting
     if (!deviceConnected && oldDeviceConnected) {
@@ -172,6 +244,7 @@ void loop() {
     // connecting
     if (deviceConnected && !oldDeviceConnected) {
 		// do stuff here on connecting
+        Serial.println("device connecting");
         oldDeviceConnected = deviceConnected;
     }
 }
