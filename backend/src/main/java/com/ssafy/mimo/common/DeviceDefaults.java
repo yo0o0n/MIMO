4b594a6e0bedbbf5b17ac8c2309c230d00d33abd
이수지 <suzy0120@naver.com>
2024-05-19 16:04:47 +0900

    fix: ãModify device default

--- backend/src/main/java/com/ssafy/mimo/common/DeviceDefaults.java
+++ backend/src/main/java/com/ssafy/mimo/common/DeviceDefaults.java
@@ -7,14 +7,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum DeviceDefaults {
 	LIGHT_WAKEUP_COLOR("FFFFFF"),
-	LIGHT_CUR_COLOR("FFFFFF"),
+	LIGHT_CUR_COLOR("E0854D"),
 	LAMP_WAKEUP_COLOR("FFFFFF"),
-	LAMP_CUR_COLOR("FFFFFF"),
+	LAMP_CUR_COLOR("E0854D"),
 	CURTAIN_OPEN_DEGREE("100"),
 	WINDOW_OPEN_DEGREE("100"),
 	WAKEUP_TIME("07:00:00"),
-	NIGHT_START_HOUR("13"),
-	NIGHT_END_HOUR("3");
+	NIGHT_START_HOUR("15"),
+	NIGHT_END_HOUR("11");
 
 	private final String value;
 }
